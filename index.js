5390ff0b86ec2ee3c8c23599a0ffa1575ab229ad
CodeSenpii <superdupa_fly@yahoo.com>
2021-10-22 21:51:44 -0400

    add scroll maual controls
    manually controll scroll with space bar

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ window.addEventListener("load", activateReader);
 
 function activateReader(){
   var counter = 0;
+  var page = 0;
   // var gold = "rgba(218,165,32,0.5)";
   var gold = "gainsboro";
   var d = document.querySelectorAll('p');
+
+  var label_id = {p1:'s2', p2:'s3', p3:'s4', p4:'s5', p5:'s6'};
+
+
   var span = document.querySelectorAll('span');
   d[0].style.opacity = 1;
   d[0].style.color = "black";
   d[0].style.backgroundColor = gold;
   d[0].classList.add("prefix", "postfix");
-
   document.addEventListener("keydown", function(e){
+    if(d[page].id && page < d.length - 1){
+      var index = Number(page);
+      document.getElementById(label_id[d[page].id]).checked = true;
+      // console.log(label_id[d[page].id]);
+    }
+
     if (e.code === "Space"){
       if(counter < d.length - 1) {
       // remove pagination from line
@@ -23,6 +33,9 @@ function activateReader(){
       d[counter+1].style.opacity = 1;
       d[counter+1].style.backgroundColor = gold;
       counter ++;
+      page ++;
+
+
     }
   } else if(e.code === 'KeyU'){
 
@@ -35,6 +48,7 @@ function activateReader(){
         // d[counter+1].style.color = "black";
         d[counter-1].style.backgroundColor = gold;
         counter --;
+        page --;
       }
     }// end elif
 
